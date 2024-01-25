9444d8528dbe62f5cecc3fcda07b894f4fb4d54f
FLRSscript <flrsscript@users.noreply.github.com>
2024-01-25 09:24:12 -0600

    asda

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const app = express();
 
 // Agregar middleware para manejar CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://musical-otter-ec6469.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Origin: *");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
